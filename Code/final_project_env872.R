cdcd4d7cfa4e1e53d7251e890e549615b3213665
Pierre Mishra <prashank.mishra@duke.edu>
2020-04-25 02:16:28 -0400

    removed large shapefile and restoring stuck commits

---
+++ Code/final_project_env872.R
@@ -0,0 +1,439 @@
+# Setting working directory
+getwd()
+#setwd("./")
+
+# Loading packages
+library("ggplot2")
+library("plyr")
+library("tidyverse")
+library("sf")
+library("ggmap")
+library("here")
+library("viridis")
+library("RColorBrewer")
+library("colormap")
+library('mapdata')
+library("modifiedmk")
+library("forecast")
+library("trend")
+library("Kendall")
+library("tseries")
+library("scico")
+library("corrplot")
+library("GGally")
+
+
+# setting ggplot theme
+peaceful.theme <- theme_classic(base_size = 14) +
+  theme(axis.text = element_text(color = "black"), 
+        legend.position = "right")
+
+# Loading tabular dataset
+df <- read.csv("Data/Raw/NID2019_U.csv")
+colnames(df) <- tolower(colnames(df)) # lowecase column headers
+
+# Removing irrelevant columns
+dam <- df %>%   
+  select(-c("other_dam_name","dam_former_name", "section",
+            "stateid", "owner_name", "dam_designer",
+            "year_modified", "inspection_date",
+            "state_reg_agency", "outlet_gates",
+            "number_of_locks", "length_of_locks",
+            "width_of_locks", "fed_funding",
+            "fed_design", "fed_construction",
+            "fed_regulatory", "fed_inspection",
+            "fed_operation", "fed_owner", 
+            "fed_other", "source_agency",
+            "submit_date","url_address","cong_name",
+            "party", "cong_dist", "otherstructureid",
+            "numseparatestructures", "permittingauthority",
+            "inspectionauthority", "jurisdictionaldam",
+            "eap_last_rev_date")) 
+
+# Correcting data types
+dam$dam_name <- as.character(dam$dam_name)
+dam$nidid <- as.character(dam$nidid)
+dam$county <- as.character(dam$county)
+dam$river <- as.character(dam$river)
+dam$city <- as.character(dam$city)
+dam$state <- as.character(dam$state)
+
+# Removing dam records for which risk potential is undetermined or 
+# not available
+dam <- droplevels(dam[!(dam$hazard=="U" | dam$hazard=="N"),])
+
+# Calculating the age of dams
+dam$age <- 2019 - dam$year_completed
+
+# Cleaning certian columns
+
+#----------------------------------------------------Exploratory data analysis
+
+# Checking NAs
+colSums(is.na(dam))
+
+# dam safety hazards
+ggplot(dam, aes(x=hazard)) +
+  geom_bar() + peaceful.theme
+
+  # hazard vs owner type ####PROBABLY NOT
+ggplot(dam) +
+  geom_bar(aes(x=hazard, fill = owner_type), position = "dodge") +
+  peaceful.theme
+
+# hazard vs private dam
+ggplot(dam) +
+  geom_bar(aes(x=hazard, fill = private_dam), position = "dodge") +
+  peaceful.theme 
+#### there are different levels with same name 'N', empty value
+
+# hazard vs dam height #### YES
+ggplot(data = dam) +
+  geom_point(aes(x = hazard, y = dam_height))
+
+#### hazard vs primary purpose
+#### only keep the first alphabet of the column "purpose"
+
+# hazard vs distance #### YES
+ggplot(dam) +
+  geom_boxplot(aes(x=hazard, y = log(distance)))
+
+# hazard vs dam length ### YES
+ggplot(dam) +
+  geom_boxplot(aes(x=hazard, y = log(dam_length)))
+
+# hazard vs structural height #### YES
+ggplot(dam) +
+  geom_boxplot(aes(x=hazard, y = log(structural_height)))
+
+# hazard vs hydraulic height #### YES
+ggplot(dam) +
+  geom_boxplot(aes(x=hazard, y = log(hydraulic_height)))
+ 
+# hazard vs surface area #### YES
+ggplot(dam) +
+  geom_boxplot(aes(x=hazard, y = log(surface_area)))
+
+# hazard vs drainage area #### YES
+ggplot(dam) +
+  geom_boxplot(aes(x=hazard, y = log(drainage_area)))
+
+# hazard vs eap #### DATA EXPLORATORY ANALYSIS
+ggplot(dam) +
+  geom_bar(aes(x=hazard, fill = eap), position = "dodge")
+
+# hazard vs inspection frequency #### YES
+ggplot(dam) +
+  geom_density(aes(x=inspection_frequency, group = hazard, fill = hazard),
+               alpha = 0.5) + peaceful.theme
+
+# hazard vs state regulated dam #### NOPE
+ggplot(dam) +
+  geom_bar(aes(x=hazard, fill = state_reg_dam), position = "dodge")
+
+# hazard vs spillway type
+ggplot(dam) +
+  geom_bar(aes(x=hazard, fill = spillway_type), position = "dodge") +
+  peaceful.theme
+# spillway type: one value is blank
+
+# hazard vs spillway width #### YES
+ggplot(dam) +
+  geom_boxplot(aes(x=hazard, y = log(spillway_width))) +
+  peaceful.theme
+
+# hazard vs volume #### HELL YEAH
+ggplot(dam) +
+  geom_boxplot(aes(x=hazard, y = log(volume)) +
+  peaceful.theme
+
+# hazard vs state  #### NO
+ggplot(dam) +
+  geom_bar(aes(x=hazard, fill = state), position = "dodge") +
+  peaceful.theme
+
+# hazard vs enforcementauthority
+ggplot(dam) +
+  geom_bar(aes(x=hazard, fill = enforcementauthority), position = "dodge") +
+  peaceful.theme
+
+# hazard vs age
+ggplot(dam) +
+  geom_boxplot(aes(x=hazard, y = age)) +
+  peaceful.theme
+
+# distribution of dam characteristics
+dam_characteristics <- dam %>%
+  select(volume, spillway_width, drainage_area, hydraulic_height,
+         surface_area, structural_height, dam_height, dam_length)
+
+for (i in ncols(dam_characteristics)) {
+  ggplot(dam)
+}
+
+ggplot(dam) +
+  geom_hist (aes(x=volume))
+
+#-------------------------------------Cleaning some columns
+
+
+
+#-------------------------------------Multinomial (Ordinal) Logistic Model
+# age, enforcementauthority, volume, spillway width, inspection frequency,
+# distance, drainage area, hydraulic height, surface area, structural height,
+# dam height, dam length
+
+# do correlation matrix
+# dam_characteristics <- dam %>%
+#   select(volume, spillway_width, drainage_area, hydraulic_height,
+#          surface_area, structural_height, dam_height, dam_length)
+# volume is not that important but lets see
+# nid height has the least missing values so lets use that
+
+dam_characteristics <- dam %>% 
+  select(volume, nid_storage, spillway_width, drainage_area, nid_height, surface_area, dam_length)
+
+# To improve skewed distribution into more of a normal distribution
+summary(is.na(dam_characteristics)) # count NA in each column
+dam_characteristics[dam_characteristics==0] = 1 # to prevent log transforming errors
+log_dam_characteristics <- log(dam_characteristics)
+log_dam_characteristics$hazard <- dam$hazard
+summary(is.na(log_dam_characteristics))
+
+log_dam_characteristics_subset <- log_dam_characteristics %>%
+  select(volume, nid_storage, spillway_width) %>%
+  drop_na
+
+# Convert Inf or Nan to NA
+# log_dam_characteristics[sapply(log_dam_characteristics, is.infinite)] <- NA
+
+
+#ggpairs(log_dam_characteristics) 
+# Strong correlation:
+# hydraulic_height & structural_height
+# hydraulic height & dam_height
+# dam height & structural height
+# log and check volume and anyone height 
+
+# Checking logged dstribution of each potential predictor variable
+for (i in length(log_dam_characteristics)) {
+  ggplot (log_dam_characteristics) +
+    geom_histogram(aes(x = colnames(log_dam_characterisitcs)[i]))
+}
+
+
+corrplot(as.matrix(log_dam_characteristics), is.corr = FALSE, upper = "ellipse")
+
+# Ordinal Logistic Regression
+
+library("MASS")
+model_1 <- polr(hazard ~ volume+nid_storage+spillway_width+drainage_area+nid_height+surface_area+dam_length, data = log_dam_characteristics, na.action = na.exclude)
+# model_2 <- polr(hazard ~ volume+nid_storage+spillway_width+drainage_area+nid_height+surface_area, data = log_dam_characteristics, na.action = na.exclude)
+# model_3 <- polr(hazard ~ volume+nid_storage+spillway_width+drainage_area+nid_height, data = log_dam_characteristics, na.action = na.exclude)
+# model_4 <- polr(hazard ~ volume+nid_storage+spillway_width+drainage_area, data = log_dam_characteristics, na.action = na.exclude)
+# model_5 <- polr(hazard ~ volume+nid_storage+spillway_width, data = log_dam_characteristics, na.action = na.exclude)
+
+summary(model_1)
+# summary(model_2)
+# summary(model_3)
+# summary(model_4)
+# summary(model_5)
+
+summary_table <- coef(summary(model_1))
+pval <- pnorm(abs(summary_table[, "t value"]),lower.tail = FALSE)* 2
+summary_table <- cbind(summary_table, "p value" = round(pval,3))
+summary_table
+
+
+head(dam)
+#-------------------------------------Monotonic trend analysis
+
+# importing gage data
+huc8 <- read.csv("Data/Raw/wy01d_col_data.txt", 
+                 sep = "\t")
+basin_nc <- st_read(here("Data", "Raw", "8Digit_HUC_Subbasins.shp"))
+
+# renaming columns
+colnames(huc8) <- sub("X", "", colnames(huc8))
+
+# selecting columns for North Carolina
+huc8_nc <- huc8 %>%
+  select(date, one_of(as.character(basin_nc$HUC_8)))
+ncolumns <- ncol(huc8_nc)
+huc8_nc$date <- as.Date(huc8_nc$date, origin = "1901-01-01")
+huc8_nc_ts <- ts(huc8_nc[2:ncolumns]) 
+plot.ts(huc8_nc_ts[,23])
+
+for (i in 1:ncol(huc8_nc_ts)){
+  test_results <- mk.test(huc8_nc_ts[,i])
+  if (test_results$p.value < 0.05) {
+    basin_nc$trend[i] <- test_results$estimates[3]*100
+  }
+  else {
+    basin_nc$trend[i] <- 0
+  }
+}
+#-------------------------------------Geospatial analysis
+
+# subsetting north carolina dams
+dam_nc <- dam[which(dam$state == "NC"),]
+dam_nc$river <- tolower(dam_nc$river)
+dam_nc <- select(dam_nc, nidid, longitude, latitude, county, river, city, hazard, state)
+dam_nc$huc8 <- 
+
+# importing NC boundary shapefile and streams shapefile
+stream_nc_raw <- st_read(here("Data", "Raw", "2014_IR_ Overall_Cat.shp"))
+stream_nc_raw$AU_NAME <- tolower(stream_nc_raw$AU_NAME)
+#colnames(stream_nc_raw)[3] <- "river"
+#### +proj=lcc +lat_1=36.16666666666666 +lat_2=34.33333333333334 +lat_0=33.75 +lon_0=-79 +x_0=609601.22 +y_0=0 +datum=NAD83 +units=m +no_defs
+state_bound_raw <- st_read(here("Data","Raw", "state_bounds.shp"))
+state_bound_raw_nc <- state_bound_raw[which(state_bound_raw$NAME == "North Carolina"),]
+#### +proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs
+
+# selecting rivers with dams
+stream_nc_raw <- subset(stream_nc_raw, AU_NAME %in% dam_nc$river)
+
+
+# setting CRS for shapefiles
+stream_nc <- stream_nc_raw
+st_crs(stream_nc) <- "+proj=lcc +lat_1=36.16666666666666 +lat_2=34.33333333333334 +lat_0=33.75 +lon_0=-79 +x_0=609601.22 +y_0=0 +datum=NAD83 +units=m +no_defs"
+stream_nc <- stream_nc %>%
+  st_set_crs(32119)
+stream_nc <- sf::st_transform(stream_nc, crs = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs") %>%
+  st_set_crs(5070)
+
+state_bound_nc <- state_bound_raw_nc
+st_crs(state_bound_nc) <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
+state_bound_nc <- state_bound_nc %>%
+  st_set_crs(5070)
+
+# projecting CRS for shapefiles
+na_albers_proj4 <- "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
+na_albers_epsg <- 102008
+
+state_bound_nc_albers <- sf::st_transform(state_bound_nc, crs = na_albers_proj4) %>%
+  st_set_crs(na_albers_epsg)
+
+# checking CRS code
+st_crs(stream_nc)
+st_crs(state_bound_nc)
+st_crs(state_bound_nc_albers)
+
+# converting tabular data to sf format
+dam_nc_geo <- st_as_sf (dam_nc, coords = c("longitude", 
+                                     "latitude"),
+                     crs = 4326, dim = "XY")
+
+
+# plotting all the shapefile layers
+# limit <- max(abs(basin_nc$trend)) * c(-1,1)
+# pdf(here("Output", "newest.pdf"), width = 11, height = 8.5)
+# ggplot() +
+#   geom_sf(data = state_bound_nc_geom, fill = NA) +
+#   geom_sf(data = basin_nc, aes(fill = trend), color = "black") +
+#   scale_fill_scico (palette = "broc", limit = limit, direction = -1) +
+#   geom_sf(data = dam_nc_geo, aes(color = hazard), size = 1.5, alpha = 0.5, show.legend = "point") +
+#   scale_color_manual(values = c("red", "green", "orange")) + 
+#   labs(x = 'Longitude', y='Latitude', title = "NC Dam Hazard and Monotonic Trend Analysis of subbasin (HUC8) annual water \nrun-off (1901-2018)\n", 
+#        color = "Dam Hazard", fill = "Trend Coefficient (%)") +
+#   theme_bw() + theme(legend.key = element_blank()) + 
+#                        peaceful.theme
+# dev.off()
+
+# 
+huc8_dam <- st_intersection(dam_nc_geo, basin_nc)
+
+ggplot(subset(huc8_dam, hazard == 'L')) +
+  geom_point(aes(x=hazard, y = trend),
+               alpha = 0.5) + peaceful.theme
+
+l <- subset(huc8_dam, hazard == 'L')
+s <- subset(huc8_dam, hazard == 'S')
+h <- subset(huc8_dam, hazard == 'H')
+
+count(l[which(l$trend == 0),]) #841
+count(l[which(l$trend > 0),]) #6
+count(l[which(l$trend < 0),]) # 484
+
+count(s[which(s$trend == 0),]) #319
+count(s[which(s$trend > 0),]) #9
+count(s[which(s$trend < 0),]) #225
+
+count(h[which(h$trend == 0),]) #776
+count(h[which(h$trend > 0),]) #17
+count(h[which(h$trend < 0),]) #514
+
+
+
+
+
+# to identify crs and epsg
+
+state_bound <- st_read(here("Data","Raw", "state_bounds.shp"))
+state_bound_nc <- state_bound[which(state_bound$NAME == "North Carolina"),]
+st_crs(state_bound_nc)
+
+
+
+proj4string
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-----------------------------------------------------BS
+us_map <- map_data("state")
+ggplot() + geom_polygon(data = us_map, aes(x = long, y = lat, fill = region, group = group),
+               color = "white") +
+  coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
+  guides (fill = FALSE) + theme_void()
+
+nc <- subset(us_map, region %in% "north carolina")
+ggplot(data = nc) +
+  geom_polygon(aes(x = long, y = lat), color='white') +
+  coord_map(projection = "albers", lat0 = 35.7596, lat1 = 79.0913) +
+  guides (fill = FALSE) + theme_void()
+
+map()
+ggplot(data = us_states,
+       mapping = aes(x = long, y = lat)) + #, group = group, fill = coefficient
+  geom_polygon(color = "gray90", size = 0.1) +
+  coord_map(projection = "albers", lat0 = 39, lat1 = 45)+
+  theme_void() + theme(legend.position = c(0.90, 0.20))
+
+# Line plot for each state
+plot_ly(final_data, x=~begin_year, y=~acute_health_based, mode = 'lines',
+        color = final_data$state_name) %>%
+  add_lines() %>%
+  layout(xaxis = list(title = "Year",
+                      zeroline = FALSE),
+         yaxis = list(title = "Acute SDWA Violations",
+                      zeroline = FALSE))
+
+
+pdf(here("outputs", "pdf_test.pdf"), width = 11, height = 8.5)
+ggplot(data = cars) +
+  geom_point(aes(x = dist, y = speed))
+dev.off()
+
+# identifying outliers in continuous data
+ggplot()
+
+# exploring variables that explain dam safety
+
+# Map
+
+# Importing gage data for NC
+
+
+#information about each column http://files.hawaii.gov/dbedt/op/gis/data/nid_dams_data_dictionary.htm#Dam_type
+#nformation on how can dams collapse https://www.fema.gov/dam-failure-information 
+#git push -f origin master
--- README.md
+++ README.md
@@ -1,2 +1,126 @@
-# Mishra_FinalProject
-This repository contains the files pertaining to the hydrologic analysis of dams and their safety in North Carolina
+# US Dam Safety and Climate Change
+
+## Summary
+
+This repository contains the files pertaining to the hydrologic analysis of dams and their safety in US and specifically, North Carolina. Despite their socio-economic benefits, dams are prone to failure which can cause massive loss of life and property. According to the National Inventory of Dams (NID), currently, there are 14,254 dams that are classified as having high risk of failure. Therefore, my first goal is to study the variables related to dams that can affect their classification of failure risk. For such purpose, I use the 2019 National Inventory of Dams dataset released by the US Army Corps of Engineers. 
+
+Moreover, I also plan to investigate the effect of precipitation and/or discharge on dam failure risk. Climate Change is increasing frequency of heaviest precipitation events across the US that can stress the physical structure of dams. For such purpose, I use USGS North Carolina stream gage data for the period of record in order to determine hydrologic trends.
+
+## Investigators
+
+Pierre Mishra
+Masters of Environmental Management, 2021
+Nicholas School of the Environment, Duke University
+
+Contact: prashank.mishra@duke.edu
+
+## Keywords
+
+dam safety, precipitation, water infrastructure, hydrology, north carolina, national inventory of dams, usgs gage data, climate change
+
+## Database Information
+
+* 2019 National Inventory of Dams, US Army Corps of Engineers,
+The NID dataset contains US dam inventory of approximately 90,000 dams. It has been further processed to contain columns of relevant variables.
+
+Accessed: March 23, 2020
+
+* North Carolina Daily Streamflow Conditions, United States Geological Survey
+Daily streamflow data obtained from USGS will be grouped to monthly data for the stations that fall within 5 miles of dams.
+
+Accessed: TBD
+
+
+## Folder structure, file formats, and naming conventions 
+
+* Code - Contains .R file for the analysis
+* Data - Contains raw and processed data sets in comma separated values and shapefile formats/
+  + Raw - Contains unedited csv and shapefiles
+  + Processed - Contains edited csv file
+* Output - Contains .rmd file for developing the report and presenting results of the analysis
+
+File names are in the following format: final_project_env872_<title>(dot)<file_format>
+
+## Metadata
+
+Source: <https://nid.sec.usace.army.mil/ords/f?p=105:21:7801154475967::NO:::>
+Column Header | Description | .R Data Type
+--------------|-------------|-----------
+dam_name | Official name of the dam | Character
+nidid | Official identification number for the dam | Character
+longitude | Longitude at dam centerline as a single value in decimal degrees, NAD83 | Number
+latitude |   Latitude at dam centerline as a single value in decimal degrees, NAD83 | Number
+county | Name of the county in which dam is located | Alphanumeric | Character
+river | Official name of the river or stream on or near which the dam is built | Character
+city | Name of the nearest downstream city, town, or village that is most likely to be affected by floods resulting from the failure of the dam | Character
+distance | Distance from the dam to the nearest affected downstream city/town/village in miles | Number
+owner_type | Code to indicate the type of owner: F for Federal;S for State; L for Local Government (defined as have taxing authority or is supported by taxes); U for Public Utility; P for Private; X for Not Listed. Codes are concatenated if the dam is owned by more than one type | Factor
+private_dam | Code indicating whether this dam is a non-federal dam located on federal property | Factor
+dam_type |  Codes, in order of importance, to indicate the type of dam: RE for Earth; ER for Rockfill; PG for Gravity; CB for Buttress; VA for Arch; MV for Multi-Arch; RC for Roller-Compacted Concrete; CN for Concrete; MS for Masonry; ST for Stone; TC for Timber Crib; OT for Other. Codes are concatenated if the dam is a combination of several types | Factor
+core | Code to indicate the position, type of watertight member and certainty, Position:F for upstream facing; H for homogeneous dam;I for core; X for unlisted/unknown; Type:A for bituminous concrete; C for concrete; E for earth; M for metal; P for plastic; X for unlisted/unknown; Certainty:K for known; Z for estimated | Factor
+foundation | Code for the material upon which dam is founded, and certainty, Foundation:R for rock; RS for rock and soil; S for soil; U for unlisted/unknown. Certainty:K for known; Z for estimated | Factor
+puroposes | Codes to indicate the current purpose(s) for which the reservoir is used:I for Irrigation; H for Hydroelectric; C for Flood Control and Storm Water Management; N for Navigation; S for Water Supply; R for Recreation; P for Fire Protection, Stock, Or Small Farm Pond; F for Fish and Wildlife Pond; D for Debris Control; T for Tailings; G for Grade Stabilization; O for Other. The order should indicate the relative decreasing importance of the purpose. Codes are concatenated if the dam has multiple purposes | Factor
+year_completed | Year (four digits) when the original main dam structure was completed.  If unknown, and reasonable estimate is unavailable, “0000” is used | Number
+dam_length | Length of the dam, in feet, which is defined as the length along the top of the dam.  This also includes the spillway, powerplant, navigation lock, fish pass, etc., where these form part of the length of the dam | Number
+dam_heigth | Height of the dam, in feet to the nearest foot , which is defined as the vertical distance between the lowest point on the crest of the dam and the lowest point in the original streambed | Number
+structural_height | Structural height of the dam, in feet to the nearest foot, which is defined as the vertical distance from the lowest point of the excavated foundation to the top of the dam | Number
+hydraulic_height |  Hydraulic height of the dam, in feet to the nearest foot , which is defined as the vertical difference between the maximum design water level and the lowest point in the original streambed | Number
+nid_height | Maximum value of dam height, structural height, and hydraulic height, in feet. Accepted as the general height of the dam | Number
+max_discharge | Number of cubic feet per second (cu ft/sec) which the spillway is capable of discharging when the reservoir is at its maximum designed water surface elevation | Number
+max_storage | Maximum storage, in acre-feet, which is defined as the total storage space in a reservoir below the maximum attainable water surface elevation, including any surcharge storage | Number
+normal_storage | Normal storage, in acre-feet, which is defined as the total storage space in a reservoir below the normal retention level, including dead and inactive storage and excluding any flood control or surcharge storage.  For normally dry flood control dams, the normal storage will be a zero value | Number
+nid_storage | Maximum value of normal storage and maximum storage.  Accepted as the general storage of the dam | Number
+surface_area | Surface area, in acres, of the impoundment at its normal retention level | Number
+drainage_area |  Drainage area of the dam, in square miles, which is defined as the area that drains to a particular point (in this case, the dam) on a river or stream | Number
+hazard | Code to indicate the potential hazard to the downstream area resulting from failure or mis-operation of the dam or facilities:L for Low; S for Significant; H for High U for Undetermined | Factor
+eap | Whether this dam has an Emergency Action Plan (EAP) developed by the dam owner. Y for Yes;N for No; NR for Not Required by submitting agency | Factor
+inspection_frequency | The scheduled frequency interval for periodic inspections, in years | Number
+state_reg_dam | Calculated field based on Permitting Authority, Inspection Authority and Enforcement Authority.  If Yes to all three authority criteria, then dam is state regulated and will be listed as Yes | Integer
+spillway_type | Code that describes the type of spillway:C for Controlled; U for Uncontrolled; N for None | Factor
+spillway_width | Width of the spillway, to the nearest foot, available for discharge when the reservoir is at its maximum designed water surface elevation.  Typically for an open channel spillway, this is the bottom width | Integer
+volume |   Total number of cubic yards occupied by the materials used in the dam structure.  Portions of powerhouse, locks, and spillways are included only if they are an integral part of the dam and required for structural stability | Number
+state | State where dam is located | Character
+enforcementauthority | Yes if the state regulatory organization has the authority to issue notices, when applicable, to require owners of dams to perform necessary maintenance or remedial work, revise operating procedures, or take other actions, including breaching dams when necessary (from the Dam Safety Act of 2006) | Factor 
+age | Years since a dams's construction completed | Number
+
+## Scripts and code
+
+The following code was used to make the processed data set. Further edits will be updated.
+
+```R
+# Removing irrelevant columns
+dam <- df %>%   
+  select(-c("other_dam_name","dam_former_name", "section",
+            "stateid", "owner_name", "dam_designer",
+            "year_modified", "inspection_date",
+            "state_reg_agency", "outlet_gates",
+            "number_of_locks", "length_of_locks",
+            "width_of_locks", "fed_funding",
+            "fed_design", "fed_construction",
+            "fed_regulatory", "fed_inspection",
+            "fed_operation", "fed_owner", 
+            "fed_other", "source_agency",
+            "submit_date","url_address","cong_name",
+            "party", "cong_dist", "otherstructureid",
+            "numseparatestructures", "permittingauthority",
+            "inspectionauthority", "jurisdictionaldam",
+            "eap_last_rev_date")) 
+
+# Correcting data types
+dam$dam_name <- as.character(dam$dam_name)
+dam$nidid <- as.character(dam$nidid)
+dam$county <- as.character(dam$county)
+dam$river <- as.character(dam$river)
+dam$city <- as.character(dam$city)
+dam$state <- as.character(dam$state)
+
+# Removing dam records for which risk potential is undetermined or not available
+dam <- droplevels(dam[!(dam$hazard=="U" | dam$hazard=="N"),])
+
+# Calculating the age of dams
+dam$age <- 2019 - dam$year_completed
+``` 
+
+## Quality assurance/quality control
+
+The NID dataset was checked for erroneous data entry. Scatterplots were plotted for continuous variables to check for outliers. The values for each column were made consistent with relevant data type of that column. 
